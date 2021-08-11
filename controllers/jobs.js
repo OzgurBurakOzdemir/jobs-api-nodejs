246457c9b4f8eecc9934888b98a9cf8d2793e3cd
Ozgur Burak Ozdemir <ozgurburakozdemir@gmail.com>
2021-08-11 13:19:38 +0300

    job update is edited

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -31,7 +31,25 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  res.send(" updateJob");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position can not be empty");
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
