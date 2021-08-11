de02a13fb185e7452a6f5a7e6569c0193619bfed
Ozgur Burak Ozdemir <ozgurburakozdemir@gmail.com>
2021-08-12 02:54:56 +0300

    cors rateLimit xss & helmet  added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
-require("express-async-errors");
 const express = require("express");
 const app = express();
-require("dotenv").config();
 
-app.use(express.json());
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+require("dotenv").config();
+require("express-async-errors");
+require("dotenv").config();
 
 // connectDB
 const connectDB = require("./db/connect");
@@ -17,6 +21,12 @@ const jobsRouter = require("./routes/jobs");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 100 }));
 
 // routes
 app.use("/api/v1/auth", authRouter);
