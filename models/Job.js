9bd3ab525af091229f3a46ba11362a0c4b397e46
Ozgur Burak Ozdemir <ozgurburakozdemir@gmail.com>
2021-08-11 08:26:06 +0300

    create job is modified

--- models/Job.js
+++ models/Job.js
@@ -17,7 +17,7 @@ const JobSchema = new mongoose.Schema(
       default: "pending",
     },
     createdBy: {
-      type: mongoose.Type.ObjectId,
+      type: mongoose.Types.ObjectId,
       ref: "User",
       required: [true, "Please provide user"],
     },
